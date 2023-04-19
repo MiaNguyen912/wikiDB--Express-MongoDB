4ec0e631237c4efb5347c80addf78800f4fa7475
Mia Nguyen <nguyendanlinh9122000@gmail.com>
2023-04-18 19:25:15 -0700

    initial set up, using mongoose to connect to mongoDB

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+//middleware
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
+app.use(express.static("public")); //use "public" directory to store static files
 
-//TODO
+//mongoose
+mongoose.connect("mongodb://localhost:27027/wikiDB") //connect to local database 'wikiDB'
+const articleSchema = {
+    title: String,
+    content: String
+}
+
+const Article = mongoose.model("Article", articleSchema); //create 'articles' collection following the articleSchema 
+
+
+//APIs
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
